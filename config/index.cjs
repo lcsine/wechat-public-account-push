458a882803a16e216383e6fb1040b38ff233c9b8
YuBin <117196984+lcsine@users.noreply.github.com>
2023-11-23 19:06:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqdKv6Tc4V2rDIl1MZycVfBdrQ2o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	0rWlsOGRphTRJJQHm85xgnDYvaiyGGtrPkH42r4YoBE',
+      useTemplateId: '0rWlsOGRphTRJJQHm85xgnDYvaiyGGtrPkH42r4YoBE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
