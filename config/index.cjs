1ede04a904bc3e09f292cf81312f1f6640dce477
YuBin <117196984+lcsine@users.noreply.github.com>
2023-11-23 19:00:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '鲍勃',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqdKv6QrepKAiyEHmWjdLUgE28s0',
+      id: 'oqdKv6Tc4V2rDIl1MZycVfBdrQ2o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'kcslLXPx6taaxP1J9R80swnK2EmmNwDiZCpn6eFJIFg',
+      useTemplateId: '	0rWlsOGRphTRJJQHm85xgnDYvaiyGGtrPkH42r4YoBE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -55,9 +55,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '404#',
+      name: 'L',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oqdKv6QrepKAiyEHmWjdLUgE28s0',
+      id: 'oqdKv6Tc4V2rDIl1MZycVfBdrQ2o',
     }
   ],
 
