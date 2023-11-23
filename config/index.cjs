703e5e3c6859e6ab7267c6e6212331ad0ce73f13
YuBin <117196984+lcsine@users.noreply.github.com>
2023-11-23 18:56:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '鲍勃',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oqdKv6QrepKAiyEHmWjdLUgE28s0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'kcslLXPx6taaxP1J9R80swnK2EmmNwDiZCpn6eFJIFg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
